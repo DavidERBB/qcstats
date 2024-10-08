90a2d95c60d0a3c7b0d2ac20c228be76907a92ac
DavidERB <145577798+DavidERBB@users.noreply.github.com>
2024-10-08 18:23:26 +0200

    Start

---
+++ qc/hydra.js
@@ -0,0 +1,29 @@
+async function getHydraAccess(steamAuth) {
+    try {
+        const response = await fetch("https://raven-api.wbagora.com/access", {
+            headers: {
+                "Content-Type": "application/json",
+                'x-hydra-api-key': '066e79be26eb42558957c1e34daabe67'
+            },
+            method: "POST",
+            body: JSON.stringify({
+                auth: { fail_on_missing: true, steam: steamAuth },
+                options: [
+                    'configuration',
+                    'achievements',
+                    'account',
+                    'profile',
+                    'notifications',
+                    'maintenance',
+                    'wb_network',
+                ],
+            })
+        })
+        const json = await response.json();
+        return (json.token);
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+export { getHydraAccess }
